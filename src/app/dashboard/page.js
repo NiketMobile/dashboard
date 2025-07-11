d90852adf548063a37df7e466da0a50abec4ef2e
Nirav <vavadiya.nirav@gmail.com>
2025-07-11 12:07:06 +0530

    ui

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 export default function DashboardPage() {
   return (
+    // <div className="bg-amber-300 w-full h-full">
     <div className="bg-amber-300 w-full h-full">
       <h1>hello wolf</h1>
     </div>
